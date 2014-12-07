5202ca087fc43646442683d25b9948b1a4148cba
Will Dages <wdages@gmail.com>
2014-12-06 22:46:39 -0500

    Update README and include required copy blocks in code files per the original BSD license request from Adafruit, the original author

--- firmware/WS2801.h
+++ firmware/WS2801.h
@@ -9,6 +9,21 @@
 // Technobly / BDub - Jan 9th, 2014              //
 //===============================================//
 
+/*****************************************************************************
+Example sketch for driving Adafruit WS2801 pixels on the Spark Core!
+  Designed specifically to work with the Adafruit RGB Pixels!
+  12mm Bullet shape ----> https://www.adafruit.com/products/322
+  12mm Flat shape   ----> https://www.adafruit.com/products/738
+  36mm Square shape ----> https://www.adafruit.com/products/683
+  These pixels use SPI to transmit the color data, and have built in
+  high speed PWM drivers for 24 bit color per pixel
+  2 pins are required to interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+*****************************************************************************/
 
 /* ========================== Adafruit_WS2801.h =========================== */
 
