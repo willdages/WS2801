be7d47f0ae04e95df281b5f8e5043806d46f88b2
Will Dages <wdages@gmail.com>
2014-12-07 00:08:10 -0500

    Get rid of a lot of unnecessary functions

--- firmware/examples/app.cpp
+++ firmware/examples/app.cpp
@@ -43,69 +43,16 @@ Adafruit_WS2801 strip = Adafruit_WS2801(numPixel);
 // the library will handle the format change.  Example:
 //Adafruit_WS2801 strip = Adafruit_WS2801(25, WS2801_GRB);
 
-
-String http_get(const char* hostname, String path);
-
-int led = D7;
-long previousMillis = 0;
-long interval = 5*60*1000;
-
-TCPClient client;
-char buffer[512];
-
 void setup() {
-
-    Serial.begin(115200);
-
-    pinMode(led, OUTPUT);
-    digitalWrite(led, LOW);
-
-    Spark.function("color", setColor);
-    Spark.function("pixel", setPixel);
-    Spark.function("wipe", wipe);
-
+    Spark.function("color", color);
     strip.begin();
-
-    // Update LED contents, to start they are all 'off'
-    strip.show();
-
 }
 
 void loop() {
 
 }
 
-int setColor(String command)
-{
-    // format is [RRR,GGG,BBB]
-    // ie red is [255,000,000]
-    int red = command.substring(1,4).toInt();
-    int green = command.substring(5,8).toInt();
-    int blue = command.substring(9,12).toInt();
-
-    colorWipe(Color(red, green, blue), 50);
-
-    return red+green+blue;
-}
-
-int setPixel(String command)
-{
-    // format is XX,[RRR,GGG,BBB]
-    // ie 1st red is 00,[255,000,000]
-    int pixel = command.substring(0,2).toInt();
-
-    int red = command.substring(4,7).toInt();
-    int green = command.substring(9,12).toInt();
-    int blue = command.substring(14,17).toInt();
-
-    strip.setPixelColor(pixel,red,green,blue);
-    strip.show();
-
-    return pixel+red+green+blue;
-}
-
-// fill the dots one after the other with said color
-int wipe(String command) {
+int color(String command) {
     // format is [RRR,GGG,BBB]
     // ie 1st red is [255,000,000]
     int red = command.substring(1,4).toInt();
@@ -118,44 +65,4 @@ int wipe(String command) {
         strip.show();
         delay(50);
     }
-}
-
-
-void colorWipe(uint32_t c, uint8_t wait) {
-  int i;
-
-  for (i=0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-/* Helper functions */
-
-// Create a 24 bit color value from R,G,B
-uint32_t Color(byte r, byte g, byte b)
-{
-  uint32_t c;
-  c = r;
-  c <<= 8;
-  c |= g;
-  c <<= 8;
-  c |= b;
-  return c;
-}
-
-//Input a value 0 to 255 to get a color value.
-//The colours are a transition r - g -b - back to r
-uint32_t Wheel(byte WheelPos)
-{
-  if (WheelPos < 85) {
-   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if (WheelPos < 170) {
-   WheelPos -= 85;
-   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
 }
